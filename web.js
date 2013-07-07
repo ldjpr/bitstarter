2d409148283d4013324212dcaf9162d366a7b21a
Leslie De Jesus <ldejesus@gmail.com>
2013-07-07 12:18:02 -0400

    read from file index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World3!');
+	fs.readFile('index.html', function (err, data) {
+	  if (err) throw err;
+	  console.log(data);
 });
 
 var port = process.env.PORT || 5000;
